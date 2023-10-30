59c5ff94a7045907cc2c3c1f2d0ef04e6ef6573b
Threetona <thteetona@mailoffice.id>
2023-10-30 21:03:41 +0700

    update repo

--- dist/src/auth/auth.controller.d.ts
+++
@@ -1,8 +0,0 @@
-import { AuthService } from './auth.service';
-import { AuthEntity } from './entity/auth.entity';
-import { LoginDto } from './dto/login.dto';
-export declare class AuthController {
-    private readonly authService;
-    constructor(authService: AuthService);
-    login({ email, password }: LoginDto): Promise<AuthEntity>;
-}
