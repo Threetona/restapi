58665861b1d80897c887a7a06ce9b1abb1d8f59d
Abiyyu Aqil <abiyyuaqils@gmail.com>
2023-11-04 21:21:28 +0700

    add endpoint menu

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,10 @@ import { PrismaModule } from './prisma/prisma.module';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
 import { RoleModule } from './role/role.module';
+import { MenuModule } from './menu/menu.module';
 
 @Module({
-    imports: [PrismaModule, AuthModule, UsersModule, RoleModule],
+    imports: [PrismaModule, AuthModule, UsersModule, RoleModule, MenuModule],
     controllers: [],
     providers: [AppService],
 })
