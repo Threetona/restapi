15fca3be441ca289d684434ca1a972a0b7855c5c
Kiki Irfansyah <kikiirfansyah33@gmail.com>
2023-11-05 17:07:24 +0700

    conflig

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,11 +8,8 @@ import { RoleModule } from './role/role.module';
 import { MenuModule } from './menu/menu.module';
 import { SubmenuModule } from './submenu/submenu.module';
 import { Submenu3Module } from './submenu3/submenu3.module';
-<<<<<<< HEAD
 import { PermissionModule } from './permission/permission.module';
-=======
 import { Submenu2Module } from './submenu2/submenu2.module';
->>>>>>> 345297577d8dc5f5ba52abd0d4be413d84b7d7ab
 
 @Module({
     imports: [
